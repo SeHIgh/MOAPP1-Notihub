f9101f916fb47dde13d889cca8df268ca9c1469a
seangolden11 <88262691+seangolden11@users.noreply.github.com>
2024-11-21 17:05:51 +0900

    handler 설정
    handler을 설정함으로 일다 디버그를 위해 10초 간격으로 서비스가 실행되게 만듬

--- android/app/src/main/java/com/example/notihub/service/AlarmService.kt
+++ android/app/src/main/java/com/example/notihub/service/AlarmService.kt
@@ -2,19 +2,31 @@ package com.example.notihub.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
 
 class AlarmService : Service() {
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val runnable = object : Runnable {
+        override fun run() {
+            // 10초마다 실행될 작업
+            Log.d("MyService", "Service running...")
+            handler.postDelayed(this, 10000)
+        }
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("AlarmService", "Service started after boot.")
-
-        // 원하는 작업을 수행
-
+        handler.post(runnable) // 주기적인 작업 시작
         return START_STICKY
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(runnable) // 작업 중지
     }
+
+    override fun onBind(intent: Intent?): IBinder? = null
 }
